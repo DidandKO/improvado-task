38b41c1e8130ad10fe89b471b98c07c83e55b11d
Did&K_O <didenko.nazar@list.ru>
2021-09-11 17:55:05 +0700

    minor edits

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ import os
 from configuration import SOURCE_ROOT, RESULT_FOLDER
 
 
-def get_info_csv(file):
-    with open(file, newline='') as csvfile:
-        csv_file_reader = csv.DictReader(csvfile)
+def get_info_csv(current_file):
+    with open(current_file, newline='') as csv_file:
+        csv_file_reader = csv.DictReader(csv_file)
         for row in csv_file_reader:
             yield row
 
 
-def get_info_json(file):
-    with open(file) as jsonfile:
-        json_file_reader = json.load(jsonfile)
+def get_info_json(current_file):
+    with open(current_file) as json_file:
+        json_file_reader = json.load(json_file)
         for row in json_file_reader['fields']:
-            # print(row)
             yield row
-        jsonfile.close()
 
 
-def get_info_xml(file):
-    with open(file) as xmlfile:
-        xml_file_reader = ET.parse(file)
+def get_info_xml(current_file):
+    with open(current_file):
+        xml_file_reader = ET.parse(current_file)
         root = xml_file_reader.getroot()
         xml_result = {}
         for tag in root.findall('objects/object'):
@@ -34,16 +32,22 @@ def get_info_xml(file):
 
 
 def create_basic_tsv(table, fieldnames):
-    with open(os.path.join(RESULT_FOLDER,'basic_result.tsv'), 'w', newline='') as tsvfile:
+    with open(os.path.join(RESULT_FOLDER, 'basic_result.tsv'), 'w', newline='') as tsv_file:
         dialect = csv.Dialect
         dialect.delimiter = '\t'
         dialect.other_param = 'val'
         dialect.quoting = csv.QUOTE_NONE
         dialect.lineterminator = '\n'
-        writer = csv.DictWriter(tsvfile, fieldnames=fieldnames, dialect=dialect)
+        writer = csv.DictWriter(tsv_file, fieldnames=fieldnames, dialect=dialect)
         writer.writeheader()
         for row in table:
             writer.writerow(row)
+
+
+'''
+    Два следующих метода необходимы для выделения общих колонок среди всех данных файлов
+    и для удаления лишней информации.
+'''
 
 
 def find_common_keys(rows):
